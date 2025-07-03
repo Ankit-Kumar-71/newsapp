b94b150f8a3ded91b16597c11f55d2d9bb3b66f9
Ankit-Kumar-71 <jeetkashyap20224@gmail.com>
2025-07-03 20:22:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Newsapp from "./Components/Newsapp";
+
+
+
+
+function App() {
+  return (
+    <>
+    <Newsapp/>
+    </>
+  );
+} 
+
+export default App;
